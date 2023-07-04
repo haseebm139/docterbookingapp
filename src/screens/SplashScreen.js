b68ae1cec68cf517665fb10a5122dba1e9c1a138
mazharaltaf123 <toobitechllc@gmail.com>
2023-07-04 19:14:52 +0500

    add

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -22,7 +22,6 @@ function SplashScreen () {
   
   return (
     <View style={styles.container}>
-      <MyStatusBar barStyle="light-content"/>
           <TouchableOpacity >
         <View >
           <Logo/>
